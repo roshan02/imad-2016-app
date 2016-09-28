ab482d2c7107d6e231897d64f7bcc9503a5681bc
roshan02 <dn.roshan2@gmail.com>
2016-09-28 17:35:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function (req, res) {
+app.get('article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'this is my article one'));
 });
-app.get('/article-two', function (req, res) {
+app.get('article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'this is my article two'));
 });
-app.get('/article-three', function (req, res) {
+app.get('article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'this is my article three'));
 });
 
