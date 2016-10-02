36326b4db5ac20e37699203600657c181fc30377
roshan02 <dn.roshan2@gmail.com>
2016-10-03 01:24:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,9 @@ app.get('/ui/JabTak.mp3', function (req, res) {
 app.get('/ui/msd.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'msd.png'));
 });
+app.get('/ui/roshan30.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'roshan30.png'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
